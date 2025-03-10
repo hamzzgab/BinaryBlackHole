475f26c515211de015029a64de534b6fa4a782f7
Hamza Gabajiwala <hamzajg16@gmail.com>
2025-03-10 23:03:18 +0000

    chore: refactor tests

--- src/test/arrays/static/test_remove_duplicates.py
+++ src/test/arrays/static/test_remove_duplicates.py
@@ -2,14 +2,30 @@ from src.main.arrays.static.remove_duplicates import remove_duplicates
 
 
 class TestRemoveDuplicates:
+
     def test_case_1(self):
         nums = [1, 1, 2]
-        assert remove_duplicates(nums) == len(set(nums))
+
+        exp_unique_nums = len(set(nums))
+        act_unique_nums = remove_duplicates(nums)
+
+        assert nums[:exp_unique_nums] == [1, 2]
+        assert exp_unique_nums == act_unique_nums
 
     def test_case_2(self):
         nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-        assert remove_duplicates(nums) == len(set(nums))
+
+        exp_unique_nums = len(set(nums))
+        act_unique_nums = remove_duplicates(nums)
+
+        assert nums[:exp_unique_nums] == [0, 1, 2, 3, 4]
+        assert exp_unique_nums == act_unique_nums
 
     def test_case_3(self):
         nums = [1, 1]
-        assert remove_duplicates(nums) == len(set(nums))
+
+        exp_unique_nums = len(set(nums))
+        act_unique_nums = remove_duplicates(nums)
+
+        assert nums[:exp_unique_nums] == [1]
+        assert exp_unique_nums == act_unique_nums
