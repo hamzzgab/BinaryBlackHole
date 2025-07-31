64e63271f6b5fa7801584bef218bff9c97a99610
Hamza Gabajiwala <hamzajg16@gmail.com>
2025-07-31 11:50:59 +0100

    Update test_merge.py

--- src/test/sorting/neet/test_merge.py
+++ src/test/sorting/neet/test_merge.py
@@ -3,5 +3,11 @@ from src.main.sorting.neet.merge import sort
 
 class TestMergeSort:
     def test_sort(self):
-        nums = [5, 4, 3, 2, 1]
-        assert sort(nums, 0, len(nums)) == [1, 2, 3, 4, 5]
+        nums_unsorted = [5, 4, 3, 2, 1]
+        nums_sorted = [1, 2, 3, 4, 5]
+        nums_len = len(nums_unsorted)
+
+        assert sort(nums_unsorted, 0, nums_len) == [1, 2, 3, 4, 5]
+
+        nums_unsorted = [1, 3, 2, 4, 5]
+        assert sort(nums_unsorted, 0, nums_len) == nums_sorted
